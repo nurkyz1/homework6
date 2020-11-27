3d1d16f82a88d8fec713d88162d71995428ec16e
nurkyz1 <nurkyz.esengulova020102@gmail.com>
2020-11-27 11:16:40 +0600

    home work1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Boss boss = new Boss();
-        boss.setWeapon(" sword ");
+        
 
         boss.setHealth(1000);
         boss.setDamage(50);
